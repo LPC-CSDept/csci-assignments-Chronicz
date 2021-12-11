f4136bc831fe367c37eb367550b4b7524cea277c
Chronicz <45631752+Chronicz@users.noreply.github.com>
2021-12-11 14:50:54 -0800

    Finals Question 1 Edit

--- ChanHei_Finals/q5.asm
+++ ChanHei_Finals/q5.asm
@@ -14,6 +14,23 @@ count:  .word   3
 
     .text
     .globl main
-main:   lw  $t0, count          # initiate input counter
+main:   lw  $t0, count              # Initiate Input Counter
+        ori $t1, $zero, 10
+        lui $t1, 0xffff             # Load Receiver Control Register
+poll:   lw  $t2, 0($t1)
+        andi    $t2, $t2, 0x0001    # Check if Receiver control Register is ready, ready = 1?
+        beq $t2, $zero, poll        # Polling until ready bit is 1
+        nop
+        lw  $s0, 4($t1)             # Read with Receiver Data Register
+
+        sub $s0, $s0, 48            # Subtract '0' to get digit in decimals
+        sub $t0, $t0, 1
+        beq $t0, $zero, print
+print:  move    $a0, $s1
+        li  $v0, 1
+        syscall
+
+        
+    
 
 # End of File
