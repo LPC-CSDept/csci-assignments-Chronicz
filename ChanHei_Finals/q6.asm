5f4477ffd73c893784c4f7a8949397d7db4ab1dd
Chronicz <45631752+Chronicz@users.noreply.github.com>
2021-12-11 18:15:16 -0800

    Finals Question2  Edit(Kernel Text)

--- ChanHei_Finals/q6.asm
+++ ChanHei_Finals/q6.asm
@@ -31,4 +31,25 @@ main:   mfc0    $a0, $12        # Get Status Register
         sw      $a0, 0($t0)     # Return Enable to Receiver Control Register
 
 loop:   j       here            # Infinite loop
+
+        .ktext  0x80000180      # Kernel text
+        sw      $v0, s1         # Kernel Register
+        sw      $a0, s2         # Kernel Register
+
+        mfc0    $k0, $13        # Access Cause Register
+        srl     $a0, $k0, 2     # Get ExecuteCode Field
+        andi    $a0, $a0, 0x1f  # Get exception ExecuteCode
+        bnez    $a0, reset      # Only process I/O when exception code is not 0
+
+        lui     $v0, 0xffff     # Access Kernel Space
+        lw      $a0, 4(v0)      # Get input into $a0 with Receiver Control Register at 0xffff0004
+        bne     $a0, 113, print # print input except 'q'
+    
+reset:  lw  $v0, s1             # Reset $v0
+        lw  $a0, s2             # Reset $a0
+        mtc0    $zero, $13      # Clear Cause Register
+        mfc0    $k0, $12        # Set Status registers
+        ori     $k0, 0x11       # Enable interrupts
+        mtc0,   $k0, $12        # Return to Status register
+        eret                    # return to epc
 # End of File
