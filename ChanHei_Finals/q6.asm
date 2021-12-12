677aad5286a1feb6d00bece7d700ee8d851ae336
Chronicz <45631752+Chronicz@users.noreply.github.com>
2021-12-11 17:44:32 -0800

    Finals Question 2 file create

---
+++ ChanHei_Finals/q6.asm
@@ -0,0 +1,17 @@
+#
+# Student Name: Chan Hei
+# File Name: q6.asm
+# Start Date: Dec 11(SAT)
+# CS21 Finals Question 2
+#
+# Decription:
+# Take the user character and print it in a console until 'q' is typed
+# Make the interrupt handler(Kernel text program)
+#
+# Elaboration:
+#
+# Declaration:
+#
+# Start
+
+# End of File
